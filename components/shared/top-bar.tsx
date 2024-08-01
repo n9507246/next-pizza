fc5a92b4cd13748547548f554d765c3a0b299566
Nikita Balashov <n9507246u@gmail.com>
2024-08-01 15:06:10 +0300

    добавлен компонент сортировки в верхнем меню

--- components/shared/top-bar.tsx
+++ components/shared/top-bar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from '@/components/shared/container';
 import { Categories } from './categories';
+import { SortPopup } from './sort-popup';
 
 interface Props {
   className?: string;
@@ -10,8 +11,8 @@ export const TopBar: React.FC<Props> = ({ className }) => {
   return (
     <div className="sticky top-0 bg-white pb-5 shadow-lg shadow-black/5">
         <Container className="flex items-center justify-between ">
-          This is stiky bar
           <Categories/>
+          <SortPopup/>
         </Container>
       </div>
   );
