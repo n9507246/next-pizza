6158002f0fcbdb33e34c49112704866983ae7ab6
Nikita Balashov <n9507246u@gmail.com>
2024-08-01 11:17:44 +0300

    левая часть header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -14,7 +14,16 @@ export const Header: React.FC<Props> = ({className}) => {
         <header className={cn('border border-b', className)}>
             
             <Container className="flex items-center justify-between py-8">
-                Header
+                
+                {/* Левая часть */}
+                <div className="">
+                    <Image src="/logo.png" width={35} height={35} alt="Logo" />
+                    <div>
+                        <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+                        <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+                    </div>
+                </div>
+                
             </Container>
         
         </header>
