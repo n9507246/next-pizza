6e417f47cf196000ac40cbfa24ce768d8771d900
Nikita Balashov <n9507246u@gmail.com>
2024-08-01 14:29:24 +0300

    добавлен заголовок на главной странице

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { Container } from "@/components/shared/container";
+import { Title } from '@/components/shared/title';
 
 export default function Home() {
   return (
     <>
       <Container className="flex items-center justify-between py-8">
-        <h1>Home</h1> 
+          <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
     </>
   );
