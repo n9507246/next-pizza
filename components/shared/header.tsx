e7eda971a1e186153a0603557fe470eef83af8f8
Nikita Balashov <n9507246u@gmail.com>
2024-08-01 12:43:48 +0300

    добавлена правая часть хедера

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -2,6 +2,8 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
 import { Container } from "@/components/shared/container";
+import { Button } from "@/components/ui";
+import { ArrowRightIcon, ShoppingCartIcon, UserIcon } from "lucide-react";
 
 
 
@@ -23,7 +25,29 @@ export const Header: React.FC<Props> = ({className}) => {
                         <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
                     </div>
                 </div>
-                
+
+                {/* Правая часть */}
+                <div className="flex items-center gap-3">
+                    
+                    <Button variant="outline" className="flex items-center gap-1">
+                        <UserIcon size={16}/>
+                        Войти
+                    </Button>
+
+                    <Button className="group relative">
+                        
+                        <b>520 Р</b>
+                        
+                        <span className="h-full w-[1px] bg-white/30 mx-3"/>
+                        
+                        <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                            <ShoppingCartIcon className="h-4 w-4 relative" strokeWidth={2} />
+                            <b>3</b>
+                        </div>
+                        <ArrowRightIcon className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+                    
+                    </Button>
+                </div>
             </Container>
         
         </header>
